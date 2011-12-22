bf96bbe3ed2e7b39e30d6ffd98c70b5147f83449
Ryan Hoshor <russianryebread@gmail.com>
2011-12-22 01:23:28 -0500

    Initial Commit

---
+++ iWordStats/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iWordStats
+//
+//  Created by Ryan Hoshor on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
