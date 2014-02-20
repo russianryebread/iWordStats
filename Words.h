4c9480e7869c1e9188154327b6b64d7b44dcd0da
Ryan Hoshor <russianryebread@gmail.com>
2014-02-20 15:52:47 -0600

    v1

---
+++ Words.h
@@ -0,0 +1,13 @@
+//
+//  Words.h
+//  iWordStats
+//
+//  Created by Ryan Hoshor on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Words : NSObject
+
+@end
---
+++ Words.m
@@ -0,0 +1,13 @@
+//
+//  Words.m
+//  iWordStats
+//
+//  Created by Ryan Hoshor on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Words.h"
+
+@implementation Words
+
+@end
